d3b2d2cf42260a5d6354a613ebb1a89ed65b93c4
tommy-2-hats <domuttel@proton.me>
2024-10-10 14:53:51 -0400

    removed ticker (animation)

--- multiCanvas.js
+++ multiCanvas.js
@@ -24,20 +24,25 @@ let obj = new PIXI.Graphics()
 // Add it to the stage to render
 app.stage.addChild(obj)
 
+
+//----------------------------------
+//-----  Create Annimation  -------- Hell yeah baby!
+//----------------------------------
+
 // Add a variable to count up the seconds our demo has been running
-let elapsed = 0.0;
+// let elapsed = 0.0;
 // Tell our application's ticker to run a new callback every frame, passing
 // in the amount of time that has passed since the last tick
 
-// Start the ticker
-app.ticker.add();
+// // Start the ticker
+// app.ticker.add();
 
-app.ticker.add((ticker) => {
-  // Add the time to our total elapsed time
-  elapsed += ticker.deltaTime;
-  // Update the sprite's X position based on the cosine of our elapsed time.  We divide
-  // by 50 to slow the animation down a bit...
-  sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-});
+// app.ticker.add((ticker) => {
+//   // Add the time to our total elapsed time
+//   elapsed += ticker.deltaTime;
+//   // Update the sprite's X position based on the cosine of our elapsed time.  We divide
+//   // by 50 to slow the animation down a bit...
+//   sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
+// });
 
 
