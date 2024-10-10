4e759ff936ad156d737659f26ad4bc83a8e5aac3
tommy-2-hats <domuttel@proton.me>
2024-10-10 12:29:44 -0400

    added shape to ticker scene

--- main.js
+++ main.js
@@ -16,10 +16,22 @@ let sprite = PIXI.Sprite.from('/assets/tiger.svg');
 // Add the sprite to the stage
 app.stage.addChild(sprite);
 
+// Create a Graphics object, draw a rectangle and fill it
+let obj = new PIXI.Graphics()
+  .rect(10, 10, 200, 100)
+  .fill(0xffff00);
+
+// Add it to the stage to render
+app.stage.addChild(obj)
+
 // Add a variable to count up the seconds our demo has been running
 let elapsed = 0.0;
 // Tell our application's ticker to run a new callback every frame, passing
 // in the amount of time that has passed since the last tick
+
+// Start the ticker
+app.ticker.add();
+
 app.ticker.add((ticker) => {
   // Add the time to our total elapsed time
   elapsed += ticker.deltaTime;
@@ -27,5 +39,5 @@ app.ticker.add((ticker) => {
   // by 50 to slow the animation down a bit...
   sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
 });
-// Start the ticker
-app.ticker.add();
+
+
