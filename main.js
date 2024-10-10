5d649dbe3d8893ada9323a52cc0d7f663b153212
tommy-2-hats <domuttel@proton.me>
2024-10-10 12:08:00 -0400

    updated module import from cherry pick to entire pixi.js lib - canvas rendered successfully

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-import { Application, Sprite, Assets } from 'pixi.js';
+import * as PIXI from 'pixi.js';
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new Application();
+const app = new PIXI.Application();
 
-// Wait for the Renderer to be available
-await app.init();
+await app.init({ width: 640, height: 360 });
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
 document.body.appendChild(app.canvas);
-
-// load the texture we need
-const texture = await Assets.load('bunny.png');
-
-// This creates a texture from a 'bunny.png' image
-const bunny = new Sprite(texture);
-
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
-
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
-
-// Listen for frame updates
-app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-});
