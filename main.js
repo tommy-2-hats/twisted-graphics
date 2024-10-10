7f695e45af58ed3fb7a81a0331e38efff057c38d
tommy-2-hats <domuttel@proton.me>
2024-10-10 12:15:01 -0400

    Added Sprite svg iamge)  to scene

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 import * as PIXI from 'pixi.js';
 
+// Create a new PIXI application
 const app = new PIXI.Application();
 
+// Initialize the app with a width and height
 await app.init({ width: 640, height: 360 });
 
+// Add the canvas to the document body
 document.body.appendChild(app.canvas);
+
+// load the PNG asynchronously
+await PIXI.Assets.load('/assets/tiger.svg');
+let sprite = PIXI.Sprite.from('/assets/tiger.svg');
+
+// Add the sprite to the stage
+app.stage.addChild(sprite);
