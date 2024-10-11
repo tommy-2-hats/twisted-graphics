83aaf811c5a1b03b88a794a9c9239dd0f6f9989c
tommy-2-hats <domuttel@proton.me>
2024-10-10 23:57:59 -0400

    all iamges loading at 0, 0

--- main.js
+++ main.js
@@ -4,23 +4,69 @@ import * as PIXI from 'pixi.js';
 const app = new PIXI.Application();
 
 // Initialize the app with a width and height
-await app.init({ resizeTo: window, resizeTo: window });
+await app.init({ resizeTo: window, antialias: true });
 
 // Add the canvas to the document body
-document.body.appendChild(app.canvas);
+document.getElementById('mainMenu').appendChild(app.canvas);
 
-// load the PNG asynchronously
-await PIXI.Assets.load('/assets/main-menu/tiger.svg');
-let sprite = PIXI.Sprite.from('/assets/main-menu/tiger.svg');
+// Load the SVGs
+const imagePaths = [
+  'assets/main-menu/swirl.svg',
+  'assets/main-menu/ux.svg',
+  'assets/main-menu/tiger.svg',
+  'assets/main-menu/logos.svg',
+  'assets/main-menu/eyes.svg',
+  'assets/main-menu/me.svg'
+];
 
-// Add the sprite to the stage
-app.stage.addChild(sprite);
+// load the SVGs asynchronously
+const swirlSvg = await PIXI.Assets.load({
+  src: imagePaths[0],
+  data: {
+      parseAsGraphicsContext: true,
+  }
+});
+const uxSvg = await PIXI.Assets.load({
+  src: imagePaths[1],
+  data: {
+      parseAsGraphicsContext: true,
+  },
+});
+const tigerSvg = await PIXI.Assets.load({
+  src: imagePaths[2],
+  data: {
+      parseAsGraphicsContext: true,
+  },
+});
+const logosSvg = await PIXI.Assets.load({
+  src: imagePaths[3],
+  data: {
+      parseAsGraphicsContext: true,
+  },
+});
+const eyesSvg = await PIXI.Assets.load({
+  src: imagePaths[4],
+  data: {
+      parseAsGraphicsContext: true,
+  },
+});
+const meSvg = await PIXI.Assets.load({
+  src: imagePaths[5],
+  data: {
+      parseAsGraphicsContext: true,
+  },
+});
 
-// Create a Graphics object, draw a rectangle and fill it
-let obj = new PIXI.Graphics()
-  .rect(10, 10, 200, 100)
-  .fill(0xffff00);
+const swirlGraphics = new PIXI.Graphics(swirlSvg);
+const uxGraphics = new PIXI.Graphics(uxSvg);
+const tigerGraphics = new PIXI.Graphics(tigerSvg);
+const logosGraphics = new PIXI.Graphics(logosSvg);
+const eyesGraphics = new PIXI.Graphics(eyesSvg);
+const meGraphics = new PIXI.Graphics(meSvg);
 
-// Add it to the stage to render
-app.stage.addChild(obj)
-
+app.stage.addChild(swirlGraphics);
+app.stage.addChild(uxGraphics);
+app.stage.addChild(tigerGraphics);
+app.stage.addChild(logosGraphics);
+app.stage.addChild(eyesGraphics);
+app.stage.addChild(meGraphics);
