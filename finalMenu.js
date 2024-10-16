2c5c59a3ba6c42efb7d14d61bf47a3c7f4170158
Chuckle Head <happymuttel@gmail.com>
2024-10-16 14:17:47 -0400

    loaded svg from filepath obj

--- finalMenu.js
+++ finalMenu.js
@@ -1,23 +1,44 @@
-import { Application, Assets, Sprite } from 'pixi.js';
+import { Application, Assets, Graphics } from 'pixi.js';
 
-// Asynchronous IIFE
 (async () => {
-  // Create a PixiJS application.
+  // Create a new application
   const app = new Application();
 
-  // Intialize the application.
-  await app.init({ background: '#1099bb', resizeTo: window });
+  // Initialize the application
+  await app.init({ antialias: true, resizeTo: window });
 
-  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-  const bunny = new Sprite(texture);
-
-  app.stage.addChild(bunny);
-
-  bunny.anchor.set(0.5)
-
-  bunny.x = app.screen.width / 2
-  bunny.y = app.screen.height / 2
-
-  // Then adding the application's canvas to the DOM body.
+  // Append the application canvas to the document body
   document.body.appendChild(app.canvas);
+  // Load the SVGs
+  const svgFilePath = [
+    'assets/main-menu/swirl.svg',
+    'assets/main-menu/ux.svg',
+    'assets/main-menu/tiger.svg',
+    'assets/main-menu/logos.svg',
+    'assets/main-menu/eyes.svg',
+    'assets/main-menu/me.svg'
+  ];
+
+  const mySvg = await Assets.load({
+    src: svgFilePath[0],
+    data: {
+      parseAsGraphicsContext: true,
+    }
+  });
+
+  const svgGraphics = new Graphics(mySvg);
+
+  // line it up as this svg is not centered
+  const svgBounds = svgGraphics.getLocalBounds();
+
+  svgGraphics.pivot.set((svgBounds.x + svgBounds.width) / 2, (svgBounds.y + svgBounds.height) / 2);
+
+  svgGraphics.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  app.stage.addChild(svgGraphics);
+
+  app.ticker.add((time) => {
+    svgGraphics.rotation += 0.01;
+    svgGraphics.scale.set(2 + Math.sin(svgGraphics.rotation));
+  });
 })();
